7d00c691be6405313912ffcabab41dd19d395e5f
Linus Kelsey <alfred.linus.kelsey@gmail.com>
2026-05-10 11:57:06 +0100

    chore: ignore fix_stow.sh

--- fix_stow.sh
+++
@@ -1,9 +0,0 @@
-cd ~/.dotfiles
-for pkg in */; do
-  inner="$pkg.config/${pkg%/}/${pkg%/}"
-  if [ -d "$inner" ]; then
-    echo "Fixing $pkg..."
-    mv "$inner"/* "$pkg.config/${pkg%/}/"
-    rmdir "$inner"
-  fi
-done
